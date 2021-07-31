7ffc54aa7589522353857ce85073045a0670028d
Sam Watts <samuel.watts@gmail.com>
2021-07-31 16:40:03 +0100

    Added command line arguments to ensure test reaches execution stage

--- Tests/ProjectAutomationTests/TaskTests.swift
+++ Tests/ProjectAutomationTests/TaskTests.swift
@@ -1,7 +1,19 @@
+import ProjectAutomation
 import XCTest
-@testable import ProjectAutomation
 
 class TaskTests: XCTestCase {
+    override class func setUp() {
+        super.setUp()
+
+        CommandLine.arguments = ["--tuist-task", "exec", "task_name"]
+    }
+
+    override func tearDown() {
+        super.tearDown()
+
+        CommandLine.arguments = []
+    }
+
     func test_errorThrownFromTaskIsHandled() {
         _ = Task { _ in
             throw NSError(domain: #file, code: #line, userInfo: nil)
